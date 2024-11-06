034da508908f9d58ebc3e6284e7b3afcc4ab5e2f
seiyaibuki0523 <Casperchen0523@gmail.com>
2024-11-06 14:57:53 +0800

    PID

---
+++ src/PidControler.cpp
@@ -0,0 +1,26 @@
+#include <PID_v1.h>
+#include <MPU6050_light.h>
+#include <Wire.h>
+
+double outMin,outMax;
+int sampleTime = 1000;
+double Setpoint, Input, Output;
+double kp = 0;
+double ki = 0;
+double kd = 0;
+bool PIDSwitch = true;
+//double OutArray[] = {0,0};
+
+
+void PidControler(double Input,double Setpoint) {
+    PID myPID(&Input, &Output, &Setpoint, kp, ki, kd, DIRECT);
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetOutputLimits(0,180);
+    myPID.SetSampleTime(50);
+    myPID.SetTunings(kp,ki,kd);
+//    OutArray[0] = myPID.Compute();
+//    OutArray[1] = myPID.GetDirection();
+//    return OutArray;
+    return myPID.Compute();
+    
+}
